43d6271807a7d334945b091865d94f21bc60a366
Ddubo <Ddubo@LAPTOP-1VS3UNKC>
2022-04-22 15:15:48 -0600

    El anexo de la clase Alumno

---
+++ src/practica/labp1/PracticaLabP1.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package practica.labp1;
+
+/**
+ *
+ * @author Ddubo
+ */
+public class PracticaLabP1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        /*
+        \n=Salto de pagina
+        */
+        System.out.println("Hola Mundo - "+(10+12.34)+"$ \n Estado:"+true+"- Habilitado");
+        System.out.print("Datos de alumnos");
+    }
+    
+}
